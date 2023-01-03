9a7e1759bcc7a0a5a557aae8204a9df9a06d030c
CrystalPharma <crystalpharmauk@gmail.com>
2023-01-03 17:05:20 +0000

    change in function

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-
-// var windSpeed = $('#wind-speed').sibling();
-// console.log (windSpeed);
-// global variables
-var city = $('#city').text();
 var description = $('#weather-description').text();
 var temperature = $('#temperature').text();
 var maxMin = $('#maxMin').text();
@@ -19,48 +14,51 @@ $('#current-date').text(date);
 var time = moment().format('HH:mm a');
 $('#current-time').text(time);
 
-
+// city variable target on input form
+var city = $('#city').val();
 //Ajax
 // API key
 var APIKey = "cbb0a4faea3b1ca0270bcb088ec725e8";
 
 // Here we are building the URL we need to query the database
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-  "q=London&appid=" + APIKey;
+// var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+//   "q=London&appid=" + APIKey;
 
-// Here we run our AJAX call to the OpenWeatherMap API
-$.ajax({
-  url: queryURL,
-  method: "GET"
-})
-  // We store all of the retrieved data inside of an object called "response"
-  .then(function (response) {
+$(document).ready(function () {
+  $('#submitWeather').click(function () {
+    var city = $('#city').val();
+    if (city != '') {
+      $.ajax({
+        url: 'https://api.openweathermap.org/data/2.5/weather?q=' + city + "&appid=" + APIKey,
+        method: "GET",
+        dataType: "jsonp",
+        success: function (data) {
+          console.log(data);
+          //response location
+          $("#cityResult").text(data.name);
+          //weather description
+          $("#weather-description").text(data.weather[0].main);
+          //temperature
+          $("#temperature").text((data.main.temp - 273.15).toFixed(0) + "℃");
+          //max min temperature
+          $("#maxMin").text((data.main.temp_max - 273.15).toFixed(0) + " / " + (data.main.temp_min - 273.15).toFixed(0) + "℃");
+          //weather icon
+          // var iconRef = "http://openweathermap.org/img/w/" +(response.weather[0].icon) + ".png";
+          // $('#weather-icon').attr('src',iconRef);
+          //wind-speed
+          $("#wind-speed").text('☴ Wind Speed ' + data.wind.speed + "km/hr");
+          //humidity
+          $("#humidity").text('⛆ Humidity ' + data.main.humidity + "%");
+          //
 
-    // Log the queryURL
-    console.log(queryURL);
-
-    // Log the resulting object
-    console.log(response);
-
-    //response location
-    $("#city").text(response.name);
-    //weather description
-    $("#weather-description").text(response.weather[0].main);
-    //temperature
-    $("#temperature").text( (response.main.temp - 273.15).toFixed(0) + "℃" );
-    //max min temperature
-    $("#maxMin").text((response.main.temp_max - 273.15).toFixed(0) + " / " + (response.main.temp_min - 273.15).toFixed(0) + "℃" );
-    //weather icon
-    // var iconRef = "http://openweathermap.org/img/w/" +(response.weather[0].icon) + ".png";
-    // $('#weather-icon').attr('src',iconRef);
-    //wind-speed
-    $("#wind-speed").text('☴ Wind Speed: ' + response.wind.speed + "km/hr");
-    //humidity
-    $("#humidity").text('⛆ Humidity: ' + response.main.humidity + "%");
-    //
-
-
+        }
+      });
+    } else {
+      $("#error").html('Please fill in a valid city')
+    }
   });
+});
 
 
-//storing src images with different ids
+
+// //storing src images with different ids
